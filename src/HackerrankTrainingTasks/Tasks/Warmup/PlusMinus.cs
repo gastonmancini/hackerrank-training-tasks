73d40e3fed500d76d9bbbb214245e8c2f3d38a7e
Gaston Mancini <gastonmancini@gmail.com>
2016-04-09 20:54:03 -0300

    Warmup - PlusMinus problem

---
+++ src/HackerrankTrainingTasks/Tasks/Warmup/PlusMinus.cs
@@ -0,0 +1,31 @@
+﻿namespace Tasks.Warmup
+{
+    public class PlusMinus
+    {
+        public void solution(int[] arr, out double positiveNumbersFraction, out double negativeNumbersFraction, out double zeroNumbersFraction)
+        {
+            int positiveNumbersCount = 0;
+            int negativeNumbersCount = 0;
+            int zeroNumbersCount = 0;
+
+            foreach (var number in arr)
+            {
+                if (number < 0)
+                {
+                    negativeNumbersCount++;
+                } else if (number > 0)
+                {
+                    positiveNumbersCount++;
+                }
+                else
+                {
+                    zeroNumbersCount++;
+                }
+            }
+
+            positiveNumbersFraction = (double) positiveNumbersCount / arr.Length;
+            negativeNumbersFraction = (double) negativeNumbersCount / arr.Length;
+            zeroNumbersFraction = (double) zeroNumbersCount / arr.Length;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Warmup/PlusMinusTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Warmup;
+
+namespace Tests.Warmup
+{
+    [TestClass]
+    public class PlusMinusTest
+    {
+        private PlusMinus _plusMinus;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _plusMinus = new PlusMinus();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void PlusMinus_Example_Test()
+        {
+            var arr = new int[6];
+
+            arr[0] = -4;
+            arr[1] = 3;
+            arr[2] = -9;
+            arr[3] = 0;
+            arr[4] = 4;
+            arr[5] = 1;
+
+            double positiveNumbersFraction;
+            double negativeNumbersFraction;
+            double zeroNumbersFraction;
+
+            _plusMinus.solution(arr, out positiveNumbersFraction, out negativeNumbersFraction, out zeroNumbersFraction);
+            
+            Assert.AreEqual((double) 3/6, positiveNumbersFraction);
+            Assert.AreEqual((double) 2/6, negativeNumbersFraction);
+            Assert.AreEqual((double) 1/6, zeroNumbersFraction);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
