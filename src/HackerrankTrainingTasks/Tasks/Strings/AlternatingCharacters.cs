5a0ed595a10d4a1e35c9aad7ec285b8085ccded3
Gaston Mancini <gastonmancini@gmail.com>
2016-04-10 19:27:49 -0300

    Strings - AlternatingCharacters

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/AlternatingCharacters.cs
@@ -0,0 +1,24 @@
+﻿namespace Tasks.Strings
+{
+    public class AlternatingCharacters
+    {
+        public int solution(string input)
+        {
+            char previousLetter = '\0';
+            int deletionsRequired = 0;
+
+            foreach (char letter in input)
+            {
+                if ((previousLetter == 'A' && letter == 'A') ||
+                    (previousLetter == 'B' && letter == 'B'))
+                {
+                    deletionsRequired++;
+                }
+
+                previousLetter = letter;
+            }
+
+            return deletionsRequired;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/AlternatingCharactersTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class AlternatingCharactersTest
+    {
+        private AlternatingCharacters _alternatingCharacters;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _alternatingCharacters = new AlternatingCharacters();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void AlternatingCharacters_Example_Test_1()
+        {
+            var input = "AAAA";
+            Assert.AreEqual(3, _alternatingCharacters.solution(input));
+        }
+
+        [TestMethod]
+        public void AlternatingCharacters_Example_Test_2()
+        {
+            var input = "BBBBB";
+            Assert.AreEqual(4, _alternatingCharacters.solution(input));
+        }
+
+        [TestMethod]
+        public void AlternatingCharacters_Example_Test_3()
+        {
+            var input = "ABABABAB";
+            Assert.AreEqual(0, _alternatingCharacters.solution(input));
+        }
+
+        [TestMethod]
+        public void AlternatingCharacters_Example_Test_4()
+        {
+            var input = "BABABA";
+            Assert.AreEqual(0, _alternatingCharacters.solution(input));
+        }
+
+        [TestMethod]
+        public void AlternatingCharacters_Example_Test_5()
+        {
+            var input = "AAABBB";
+            Assert.AreEqual(4, _alternatingCharacters.solution(input));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
