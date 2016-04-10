e6e7a651c00597b952d5d9c16d4af7322258338f
Gaston Mancini <gastonmancini@gmail.com>
2016-04-09 21:34:34 -0300

    Warmup - TimeConversion problem

---
+++ src/HackerrankTrainingTasks/Tasks/Warmup/TimeConversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Tasks.Warmup
+{
+    public class TimeConversion
+    {
+        public string solution(string twelveHourFormatDate)
+        {
+            DateTime twentyFourHourFormatDate;
+
+            if (DateTime.TryParseExact(twelveHourFormatDate, "hh:mm:sstt", CultureInfo.InvariantCulture, DateTimeStyles.None, out twentyFourHourFormatDate))
+            {
+                return twentyFourHourFormatDate.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+            }
+
+            throw new ArgumentException("Invalid date argument format.");
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Warmup/TimeConversionTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Warmup;
+
+namespace Tests.Warmup
+{
+    [TestClass]
+    public class TimeConversionTest
+    {
+        private TimeConversion _timeConversion;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _timeConversion = new TimeConversion();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void TimeConversion_Example_Test()
+        {
+            var expectedResult = "19:05:45";
+
+            var result = _timeConversion.solution("07:05:45PM");
+            
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
