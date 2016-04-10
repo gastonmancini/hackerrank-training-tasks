901c9f0091abda133a4b0f1d839000fe2e1ea659
Gaston Mancini <gastonmancini@gmail.com>
2016-04-10 20:17:10 -0300

    Strings - Gemstones

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/Gemstones.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class Gemstones
+    {
+        public int solution(string[] rocks)
+        {
+            var intersection = rocks[0].ToCharArray();
+
+            for (int i = 1; i < rocks.Length; i++)
+            {
+                intersection = intersection.Intersect(rocks[i].ToCharArray()).ToArray();
+            }
+            
+            return intersection.Length;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/GemstonesTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class GemstonesTest
+    {
+        private Gemstones _gemstones;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _gemstones = new Gemstones();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Gemstones_Example_Test()
+        {
+            var strings = new[]
+            {
+                "abcdde", "baccd", "eeabg"
+            };
+
+            Assert.AreEqual(2, _gemstones.solution(strings));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
