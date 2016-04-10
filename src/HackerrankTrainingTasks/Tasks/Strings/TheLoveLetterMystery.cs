06f8f7d1f9e4d4eb583337034f873be2b75c3e6d
Gaston Mancini <gastonmancini@gmail.com>
2016-04-10 19:58:40 -0300

    Strings - The Love-Letter Mystery

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/TheLoveLetterMystery.cs
@@ -0,0 +1,40 @@
+﻿namespace Tasks.Strings
+{
+    public class TheLoveLetterMystery
+    {
+        public int solution(string input)
+        {
+            int operationsRequired = 0;
+
+            var textArray = input.ToCharArray();
+
+            for (int i = 0; i < textArray.Length / 2; i++)
+            {
+                operationsRequired += CalculateOperations(textArray[i], textArray[textArray.Length - 1 - i]);
+            }
+            
+            return operationsRequired;
+        }
+
+        private int CalculateOperations(char c1, char c2)
+        {
+            if (c1 > c2) return c1 - c2;
+
+            if (c1 < c2) return c2 - c1;
+
+            return 0;
+        }
+
+        /*private bool isPalindrome(string text)
+        {
+            var textArray = text.ToCharArray();
+
+            for (int i = 0; i < textArray.Length / 2; i++)
+            {
+                if (textArray[i] != textArray[textArray.Length - 1 - i]) return false;
+            }
+
+            return true;
+        }*/
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/TheLoveLetterMysteryTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class TheLoveLetterMysteryTest
+    {
+        private TheLoveLetterMystery _theLoveLetterMystery;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _theLoveLetterMystery = new TheLoveLetterMystery();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void TheLoveLetterMystery_Example_Test_1()
+        {
+            Assert.AreEqual(2, _theLoveLetterMystery.solution("abc"));
+        }
+
+        [TestMethod]
+        public void TheLoveLetterMystery_Example_Test_2()
+        {
+            Assert.AreEqual(0, _theLoveLetterMystery.solution("abcba"));
+        }
+
+        [TestMethod]
+        public void TheLoveLetterMystery_Example_Test_3()
+        {
+            Assert.AreEqual(4, _theLoveLetterMystery.solution("abcd"));
+        }
+
+        [TestMethod]
+        public void TheLoveLetterMystery_Example_Test_4()
+        {
+            Assert.AreEqual(2, _theLoveLetterMystery.solution("cba"));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
