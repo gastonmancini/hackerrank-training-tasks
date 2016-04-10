c06b9e4fc22319dfbfde85808cfeedd73ab7c21b
Gaston Mancini <gastonmancini@gmail.com>
2016-04-10 19:09:35 -0300

    Strings - Panagrams

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/Pangrams.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class Pangrams
+    {
+        // Returns true if the phrase is panagram, false otherwise
+        public bool solution(string phrase)
+        {
+            phrase = string.Join("", phrase.Replace(" ", "").ToLowerInvariant().Distinct());
+
+            if (phrase.Length != 26) return false;
+
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                phrase = phrase.Replace(letter.ToString(), "");
+            }
+
+            return string.IsNullOrEmpty(phrase);
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/PangramsTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class PangramsTest
+    {
+        private Pangrams _pangrams;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _pangrams = new Pangrams();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Pangrams_Example_Test_Is_Panagram()
+        {
+            var phrase = "The quick brown fox jumps over the lazy dog";
+            Assert.IsTrue(_pangrams.solution(phrase));
+        }
+
+        [TestMethod]
+        public void Pangrams_Example_Test_Is_Panagram_2()
+        {
+            var phrase = "We promptly judged antique ivory buckles for the next prize";
+            Assert.IsTrue(_pangrams.solution(phrase));
+        }
+
+        [TestMethod]
+        public void Pangrams_Example_Test_Is_Not_Panagram()
+        {
+            var phrase = "We promptly judged antique ivory buckles for the prize";
+            Assert.IsFalse(_pangrams.solution(phrase));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
