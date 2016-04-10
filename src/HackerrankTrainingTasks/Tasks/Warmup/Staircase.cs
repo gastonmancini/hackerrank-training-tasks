cb79eb2eace1e42c0fcea59a167d7e3b28e2bf9d
Gaston Mancini <gastonmancini@gmail.com>
2016-04-09 21:17:37 -0300

    Warmup - Staircase problem

---
+++ src/HackerrankTrainingTasks/Tasks/Warmup/Staircase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tasks.Warmup
+{
+    public class Staircase
+    {
+        public string solution(int N)
+        {
+            var result = string.Empty;
+            
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    if (i + j <= N)
+                    {
+                        result += " ";
+                    }
+                    else
+                    {
+                        result += "#";
+                    }
+                }
+                result += Environment.NewLine;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Warmup/StaircaseTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Warmup;
+
+namespace Tests.Warmup
+{
+    [TestClass]
+    public class StaircaseTest
+    {
+        private Staircase _staircase;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _staircase = new Staircase();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Staircase_Example_Test()
+        {
+            var expectedResult = "     #" + Environment.NewLine +
+                                 "    ##" + Environment.NewLine +
+                                 "   ###" + Environment.NewLine +
+                                 "  ####" + Environment.NewLine +
+                                 " #####" + Environment.NewLine +
+                                 "######" + Environment.NewLine;
+
+            var result = _staircase.solution(6);
+            
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
