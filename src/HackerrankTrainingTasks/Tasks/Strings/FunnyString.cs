1f1f5a7138a5bc38f8700aa8f3172489d0e43bfe
Gaston Mancini <gastonmancini@gmail.com>
2016-04-10 21:23:33 -0300

    Strings - FunnyString

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/FunnyString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class FunnyString
+    {
+        public bool solution(string text)
+        {
+            var reverse = text.Reverse().ToArray();
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (Math.Abs(text[i] - text[i-1]) != Math.Abs(reverse[i] - reverse[i - 1])) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/FunnyStringTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class FunnyStringTest
+    {
+        private FunnyString _funnyString;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _funnyString = new FunnyString();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void FunnyString_Example_Test_Is_Funny()
+        {
+            Assert.IsTrue(_funnyString.solution("acxz"));
+        }
+
+        [TestMethod]
+        public void FunnyString_Example_Test_Is_Not_Funny()
+        {
+            Assert.IsFalse(_funnyString.solution("bcxz"));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
