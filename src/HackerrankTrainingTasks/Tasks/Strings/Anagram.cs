ad3ca27bc6aa8cbe17b14dbc996fe54c57d7fe6c
Gaston Mancini <gastonmancini@gmail.com>
2016-04-10 22:26:11 -0300

    Strings - Anagram

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/Anagram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class Anagram
+    {
+        public int solution(string text)
+        {
+            if (text.Length%2 != 0) return -1;
+
+            var s1 = text.Substring(0, text.Length/2).ToCharArray();
+            var s2 = text.Substring(text.Length/2, text.Length/2).ToCharArray();
+
+            var letters = new int[26];
+
+            // Count number of ocurrences of characters in the first string
+            foreach (char c in s1)
+            {
+                int position = c - 'a';
+                letters[position]++;
+            }
+
+            // Decrease the ocurrences of the letters in the second string
+            foreach (char c in s2)
+            {
+                int position = c - 'a';
+                if (letters[position] > 0)
+                {
+                    letters[position]--;
+                }
+            }
+
+            return letters.Sum(Math.Abs);
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/AnagramTest.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class AnagramTest
+    {
+        private Anagram _anagram;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _anagram = new Anagram();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Anagram_Example_Test_1()
+        {
+            Assert.AreEqual(3, _anagram.solution("aaabbb"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_2()
+        {
+            Assert.AreEqual(1, _anagram.solution("ab"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_3()
+        {
+            Assert.AreEqual(-1, _anagram.solution("abc"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_4()
+        {
+            Assert.AreEqual(2, _anagram.solution("mnop"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_5()
+        {
+            Assert.AreEqual(0, _anagram.solution("xyyx"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_6()
+        {
+            Assert.AreEqual(1, _anagram.solution("xaxbbbxx"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_7()
+        {
+            Assert.AreEqual(-1, _anagram.solution("ahrlclerknxtwayoiqbrpfdemcxqmvhmcbzgmdayqxsnasdkigcznfdajcsvycjadijrmjqeiktieztohxnvesvnvpktrtvwbeqppiwyuriyqdmyazterqtmhdhzwdxahnrzv"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_8()
+        {
+            Assert.AreEqual(-1, _anagram.solution("xphhpxlbxlassuklisrqtpvqpcgjddyavfkkfvmci"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_9()
+        {
+            Assert.AreEqual(23, _anagram.solution("vakhotzflsuglmkrxowvqrbaegxskgghgrpwmodzjafwornlhlgydjajrybeehlmaajoroncouyconnvywtdfumzsndyxokzrvqkkfoaamdqbslzoffwbtvvjatiqfhjbztnfjnhvsxwliyzpfxqyunjuirnobwrasehbtozmlvxuvylawebsr"));
+        }
+
+        [TestMethod]
+        public void Anagram_Example_Test_10()
+        {
+            Assert.AreEqual(13, _anagram.solution("paecqhyjhsnpuifvurqtnosyqqctkwgzykborcxawmrswxnrogmdugbkygejckiawmopqnpmbjhxguqwadbu"));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
