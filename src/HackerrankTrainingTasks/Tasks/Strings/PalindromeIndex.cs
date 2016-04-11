7afa0d21b26de0f7eadf74c2eb4950849fb42e58
Gaston Mancini <gastonmancini@gmail.com>
2016-04-10 22:59:17 -0300

    Strings - PalindromeIndex - O(N^2)

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/PalindromeIndex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class PalindromeIndex
+    {
+        // TODO Improve solution - Current complexity = O(N^2)
+        public int solution(string text)
+        {
+            if (isPalindrome(text.ToList())) return -1;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var charList = text.ToList();
+                charList.RemoveAt(i);
+                if (isPalindrome(charList)) return i;
+            }
+
+            return 0;
+        }
+
+        private bool isPalindrome(List<char> charList)
+        {
+            for (int i = 0; i < charList.Count / 2; i++)
+            {
+                if (charList[i] != charList[charList.Count - 1 - i]) return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/PalindromeIndexTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class PalindromeIndexTest
+    {
+        private PalindromeIndex _palindromeIndex;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _palindromeIndex = new PalindromeIndex();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void PalindromeIndex_Example_Test_1()
+        {
+            Assert.AreEqual(3, _palindromeIndex.solution("aaab"));
+        }
+
+        [TestMethod]
+        public void PalindromeIndex_Example_Test_2()
+        {
+            Assert.AreEqual(0, _palindromeIndex.solution("baa"));
+        }
+
+        [TestMethod]
+        public void PalindromeIndex_Example_Test_3()
+        {
+            Assert.AreEqual(-1, _palindromeIndex.solution("aaa"));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
