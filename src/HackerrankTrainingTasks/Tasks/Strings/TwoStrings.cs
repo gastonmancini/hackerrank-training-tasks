7c363de74c35535e8ebc0ad1215ce6414812c6e8
Gaston Mancini <gastonmancini@gmail.com>
2016-04-30 16:25:16 -0300

    Strings - TwoStrings

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/TwoStrings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class TwoStrings
+    {
+        public bool solution(string firstString, string secondString)
+        {
+            var indexedSecondString = new HashSet<char>(secondString);
+            return firstString.Any(indexedSecondString.Contains);
+        }
+
+        // Terminated due to timeout
+        //public bool solution(string firstString, string secondString)
+        //{
+        //    return firstString.Any(secondString.Contains);
+        //}
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/TwoStringsTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class TwoStringsTest
+    {
+        private TwoStrings _twoStrings;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _twoStrings = new TwoStrings();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void PalindromeIndex_Example_Test_1()
+        {
+            Assert.IsTrue(_twoStrings.solution("hello", "world"));
+        }
+
+        [TestMethod]
+        public void PalindromeIndex_Example_Test_2()
+        {
+            Assert.IsFalse(_twoStrings.solution("hi", "world"));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
