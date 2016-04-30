4b572cf57a58a7333695c487ee1b9e36d6dc5650
Gaston Mancini <gastonmancini@gmail.com>
2016-04-30 16:53:26 -0300

    Strings - GameofThronesI

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/GameofThronesI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class GameofThronesI
+    {
+        public bool solution(string input)
+        {
+            var letterCount = new Dictionary<char, int>();
+
+            foreach (var letter in input)
+            {
+                var count = 1;
+
+                if (letterCount.ContainsKey(letter))
+                {
+                    count += letterCount[letter];
+                }
+
+                letterCount[letter] = count;
+            }
+
+            var oddCount = letterCount.Count(lc => lc.Value%2 == 1);
+            return oddCount == 0 || oddCount == 1;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/GameofThronesITest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class GameofThronesITest
+    {
+        private GameofThronesI _gameofThronesI;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _gameofThronesI = new GameofThronesI();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void GameofThronesI_Example_Test_1()
+        {
+            Assert.IsTrue(_gameofThronesI.solution("aaabbbb"));
+        }
+
+        [TestMethod]
+        public void GameofThronesI_Example_Test_2()
+        {
+            Assert.IsFalse(_gameofThronesI.solution("cdefghmnopqrstuvw"));
+        }
+
+        [TestMethod]
+        public void GameofThronesI_Example_Test_3()
+        {
+            Assert.IsTrue(_gameofThronesI.solution("cdcdcdcdeeeef"));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
