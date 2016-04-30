99824861ce478f2c67457a1a5878aaf824530be5
Gaston Mancini <gastonmancini@gmail.com>
2016-04-30 17:06:12 -0300

    Strings - MakeItAnagram

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/MakeItAnagram.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Strings
+{
+    public class MakeItAnagram
+    {
+        public int solution(string firstString, string secondString)
+        {
+            var firstStringLettersCount = new Dictionary<char, int>();
+
+            foreach (var letter in firstString)
+            {
+                var count = 1;
+                if (firstStringLettersCount.ContainsKey(letter))
+                {
+                    count += firstStringLettersCount[letter];
+                }
+                firstStringLettersCount[letter] = count;
+            }
+
+            var deletionsRequired = 0;
+
+            foreach (var letter in secondString)
+            {
+                if (firstStringLettersCount.ContainsKey(letter))
+                {
+                    if (firstStringLettersCount[letter] == 1)
+                    {
+                        firstStringLettersCount.Remove(letter);
+                        continue;
+                    }
+                    firstStringLettersCount[letter]--;
+                }
+                else
+                {
+                    deletionsRequired++;
+                }
+            }
+            
+            deletionsRequired += firstStringLettersCount.Values.Sum(x => x);
+
+            return deletionsRequired;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/MakeItAnagramTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class MakeItAnagramTest
+    {
+        private MakeItAnagram _makeItAnagram;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _makeItAnagram = new MakeItAnagram();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void MakeItAnagram_Example_Test_1()
+        {
+            Assert.AreEqual(4, _makeItAnagram.solution("cde", "abc"));
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
