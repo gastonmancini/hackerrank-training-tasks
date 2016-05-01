e45cb8bb8e08cb85709b5b8ed7ad1e456f8f9616
Gaston Mancini <gastonmancini@gmail.com>
2016-05-01 18:17:56 -0300

    Sorting - CorrectnessAndTheLoopInvariant

---
+++ src/HackerrankTrainingTasks/Tasks/Sorting/CorrectnessAndTheLoopInvariant.cs
@@ -0,0 +1,23 @@
+﻿namespace Tasks.Sorting
+{
+    public class CorrectnessAndTheLoopInvariant
+    {
+        /// Challenge: In the InsertionSort code below, there is an error. Can you fix it?
+        public int[] solution(int[] A)
+        {
+            var j = 0;
+            for (var i = 1; i < A.Length; i++)
+            {
+                var value = A[i]; // Stage Initialization - OK
+                j = i - 1;
+                while (j >= 0 && value < A[j]) // Stage Maintenance  - FAIL -  j >= 0 instead of j > 0
+                {
+                    A[j + 1] = A[j];
+                    j = j - 1;
+                }
+                A[j + 1] = value;
+            } // Stage Termination  - OK
+            return A;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Sorting/CorrectnessAndTheLoopInvariantTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Sorting;
+
+namespace Tests.Sorting
+{
+    [TestClass]
+    public class CorrectnessAndTheLoopInvariantTest
+    {
+        private CorrectnessAndTheLoopInvariant _correctnessAndTheLoopInvariant;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _correctnessAndTheLoopInvariant = new CorrectnessAndTheLoopInvariant();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void CorrectnessAndTheLoopInvariant_Example_Test_1()
+        {
+            var arr = new [] { 1, 4, 3, 5, 6, 2 };
+
+            var expectedResult = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var result = _correctnessAndTheLoopInvariant.solution(arr);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void CorrectnessAndTheLoopInvariant_Example_Test_2()
+        {
+            var arr = new[] { 4, 3, 2, 1};
+
+            var expectedResult = new[] { 1, 2, 3, 4 };
+
+            var result = _correctnessAndTheLoopInvariant.solution(arr);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
