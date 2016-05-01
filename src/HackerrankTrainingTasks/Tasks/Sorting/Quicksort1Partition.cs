82acb6099e3413ff92b43820412db87ed9c12eab
Gaston Mancini <gastonmancini@gmail.com>
2016-05-01 18:38:18 -0300

    Sorting - Quicksort1Partition

---
+++ src/HackerrankTrainingTasks/Tasks/Sorting/Quicksort1Partition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Sorting
+{
+    public class Quicksort1Partition
+    {
+        public int[] solution(int[] arr)
+        {
+            var left = new List<int>();
+            var equal = new List<int>();
+            var right = new List<int>();
+
+            var pivot = arr[0];
+
+            foreach (var number in arr)
+            {
+                if (number < pivot) left.Add(number);
+                if (number == pivot) equal.Add(number);
+                if (number > pivot) right.Add(number);
+            }
+
+            return left.Concat(equal).Concat(right).ToArray();
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Sorting/Quicksort1PartitionTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Sorting;
+
+namespace Tests.Sorting
+{
+    [TestClass]
+    public class Quicksort1PartitionTest
+    {
+        private Quicksort1Partition _quicksort1Partition;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _quicksort1Partition = new Quicksort1Partition();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Quicksort1Partition_Example_Test_1()
+        {
+            var arr = new [] { 4, 5, 3, 7, 2 };
+
+            var expectedResult = new[] { 3, 2, 4, 5, 7 };
+
+            var result = _quicksort1Partition.solution(arr);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
