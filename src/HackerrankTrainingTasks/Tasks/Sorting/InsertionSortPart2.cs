2bb37f4c2fdbc0369feae9982a5e4bd69b5236a5
Gaston Mancini <gastonmancini@gmail.com>
2016-05-01 17:41:59 -0300

    Sorting - InsertionSortPart2

---
+++ src/HackerrankTrainingTasks/Tasks/Sorting/InsertionSortPart2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Tasks.Sorting
+{
+    /// <summary>
+    /// Insertion sort is an elementary sorting algorithm that sorts one element at a time. 
+    /// The algorithm takes an element from the list and places it in the correct location in the list.
+    /// This process is repeated until there are no more unsorted items in the list. The computational complexity for insertion sort is O(n2).
+    /// </summary>
+    public class InsertionSortPart2
+    {
+        public IList<int[]> solution(int[] arr)
+        {
+            var shifts = new List<int[]>();
+
+            for (var counter = 0; counter < arr.Length - 1; counter++)
+            {
+                for (var index = counter + 1; index > 0; index--)
+                {
+                    if (arr[index - 1] <= arr[index]) continue;
+
+                    var aux = arr[index - 1];
+                    arr[index - 1] = arr[index];
+                    arr[index] = aux;
+                }
+
+                shifts.Add((int[])arr.Clone());
+            }
+
+            return shifts;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Sorting/InsertionSortPart2Test.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Sorting;
+
+namespace Tests.Sorting
+{
+    [TestClass]
+    public class InsertionSortPart2Test
+    {
+        private InsertionSortPart2 _insertionSortPart2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _insertionSortPart2 = new InsertionSortPart2();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void InsertionSortPart2_Example_Test_1()
+        {
+            var arr = new [] { 1, 4, 3, 5, 6, 2 };
+
+            var shifts = new List<int[]>
+            {
+                new [] {1, 4, 3, 5, 6, 2},
+                new [] {1, 3, 4, 5, 6, 2 },
+                new [] {1, 3, 4, 5, 6, 2 },
+                new [] {1, 3, 4, 5, 6, 2 },
+                new [] {1, 2, 3, 4, 5, 6 }
+            };
+
+            var result = _insertionSortPart2.solution(arr);
+
+            for (var i = 0; i < result.Count; i++)
+            {
+                CollectionAssert.AreEqual(shifts[i], result[i]);
+            }
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
