ab9f51f75243ed22f3335ff35ac5fb5183a891ba
Gaston Mancini <gastonmancini@gmail.com>
2017-01-23 22:02:27 -0300

    Sorting - Quicksort2Sorting

---
+++ src/HackerrankTrainingTasks/Tasks/Sorting/Quicksort2Sorting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Sorting
+{
+    public class Quicksort2Sorting
+    {
+        public int[] solution(int[] arr)
+        {
+            if (arr.Length == 0) return new int[0];
+
+            if (arr.Length == 2)
+            {
+                if (arr[0] > arr[1])
+                {
+                    var aux = arr[1];
+                    arr[1] = arr[0];
+                    arr[0] = aux;
+                }
+                return arr;
+            }
+
+            var left = new List<int>();
+            var equal = new List<int>();
+            var right = new List<int>();
+
+            var pivot = arr[0];
+
+            foreach (var number in arr)
+            {
+                if (number < pivot) left.Add(number);
+                if (number == pivot) equal.Add(number);
+                if (number > pivot) right.Add(number);
+            }
+
+            return solution(left.ToArray()).Concat(equal.ToArray()).Concat(solution(right.ToArray())).ToArray();
+        }
+        
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Sorting/Quicksort2SortingTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Sorting;
+
+namespace Tests.Sorting
+{
+    [TestClass]
+    public class Quicksort2SortingTest
+    {
+        private Quicksort2Sorting _quicksort2Sorting;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _quicksort2Sorting = new Quicksort2Sorting();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Quicksort2Sorting_Example_Test_1()
+        {
+            var arr = new [] { 4, 5, 3, 7, 2 };
+
+            var expectedResult = new[] { 2, 3, 4, 5, 7 };
+
+            var result = _quicksort2Sorting.solution(arr);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Quicksort2Sorting_Example_Test_2()
+        {
+            var arr = new[] { 5, 8, 1, 3, 7, 9, 2 };
+
+            var expectedResult = new[] { 1, 2, 3, 5, 7, 8, 9 };
+
+            var result = _quicksort2Sorting.solution(arr);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+    }
+}
