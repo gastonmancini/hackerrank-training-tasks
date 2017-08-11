183cb202d8d3a44d5053f0e072a4b570d45615a4
Gaston Mancini <gastonmancini@gmail.com>
2017-08-10 22:28:57 -0300

    Warmup -MiniMaxSum

---
+++ src/HackerrankTrainingTasks/Tasks/Warmup/MiniMaxSum.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasks.Warmup
+{
+    public class MiniMaxSum
+    {
+        public string solution(int[] fiveNumbers)
+        {
+            long sum = 0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            foreach (var number in fiveNumbers)
+            {
+                sum += number;
+                if (number < min) min = number;
+                if (number > max) max = number;
+            }
+
+            return $"{sum - max} {sum - min}";
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Warmup/MiniMaxSumTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Warmup;
+
+namespace Tests.Warmup
+{
+    [TestClass]
+    public class MiniMaxSumTest
+    {
+        private MiniMaxSum _miniMaxSum;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _miniMaxSum = new MiniMaxSum();
+        }
+
+        [TestMethod]
+        public void MiniMaxSum_Example_Test()
+        {
+            var numbers = new[] {1, 2, 3, 4, 5};
+
+            var result = _miniMaxSum.solution(numbers);
+
+            Assert.AreEqual("10 14", result);
+        }
+    }
+}
