4972feabb0b674daf3324984989ecc36fab2b5f3
Gaston Mancini <gastonmancini@gmail.com>
2017-08-11 17:58:24 -0300

    Strings - SuperReducedString

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/SuperReducedString.cs
@@ -0,0 +1,23 @@
+﻿namespace Tasks.Strings
+{
+    public class SuperReducedString
+    {
+        public string solution(string s)
+        {
+            if (s.Length == 1) return s;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i - 1] == s[i])
+                {
+                    s = s.Remove(i - 1, 2);
+                    i = 0;
+                }
+            }
+
+            if (s.Length == 0) return "Empty String";
+
+            return s;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/SuperReducedStringTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class SuperReducedStringTest
+    {
+        private SuperReducedString _superReducedString;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _superReducedString = new SuperReducedString();
+        }
+
+        [TestMethod]
+        public void SuperReducedString_Example_Test1()
+        {
+            var result = _superReducedString.solution("aaabccddd");
+
+            Assert.AreEqual("abd", result);
+        }
+
+        [TestMethod]
+        public void SuperReducedString_Example_Test2()
+        {
+            var result = _superReducedString.solution("baab");
+
+            Assert.AreEqual("Empty String", result);
+        }
+
+        [TestMethod]
+        public void SuperReducedString_Example_Test3()
+        {
+            var result = _superReducedString.solution("aa");
+
+            Assert.AreEqual("Empty String", result);
+        }
+    }
+}
