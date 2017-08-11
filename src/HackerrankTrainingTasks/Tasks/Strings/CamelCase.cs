6534cdb8da8b641093509a2935098c7fe82e07c5
Gaston Mancini <gastonmancini@gmail.com>
2017-08-11 18:40:49 -0300

    Strings - CamelCase

---
+++ src/HackerrankTrainingTasks/Tasks/Strings/CamelCase.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasks.Strings
+{
+    public class CamelCase
+    {
+        public int solution(string s)
+        {
+            int count = 1;
+
+            foreach (var character in s)
+            {
+                // Uppercase letters are between 65 and 90 in ASCII
+                if (character >= 65 && character <= 90) 
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Strings/CamelCaseTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Strings;
+
+namespace Tests.Strings
+{
+    [TestClass]
+    public class CamelCaseTest
+    {
+        private CamelCase _camelCase;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _camelCase = new CamelCase();
+        }
+
+        [TestMethod]
+        public void CamelCase_Example_Test_1()
+        {
+            var input = "saveChangesInTheEditor";
+
+            var result = _camelCase.solution(input); 
+
+            Assert.AreEqual(5, result);
+        }
+       
+    }
+}
