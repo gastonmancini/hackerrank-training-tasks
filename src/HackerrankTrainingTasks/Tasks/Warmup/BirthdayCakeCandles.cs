18ceb83bbc1521af746e5c5ecd63afa48a5e0309
Gaston Mancini <gastonmancini@gmail.com>
2017-08-11 16:53:12 -0300

    Warmup - BirthdayCakeCandles

---
+++ src/HackerrankTrainingTasks/Tasks/Warmup/BirthdayCakeCandles.cs
@@ -0,0 +1,26 @@
+﻿namespace Tasks.Warmup
+{
+    public class BirthdayCakeCandles
+    {
+        public int solution(int n, int[] ar)
+        {
+            int maximumCandleHeight = int.MinValue;
+            int maximumCandleHeightCount = 0;
+
+            foreach (var candleHeight in ar)
+            {
+                if (maximumCandleHeight < candleHeight)
+                {
+                    maximumCandleHeight = candleHeight;
+                    maximumCandleHeightCount = 1;
+                }
+                else if (maximumCandleHeight == candleHeight)
+                {
+                    maximumCandleHeightCount++;
+                }
+            }
+
+            return maximumCandleHeightCount;
+        }
+    }
+}
---
+++ src/HackerrankTrainingTasks/Tests/Warmup/BirthdayCakeCandlesTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks.Warmup;
+
+namespace Tests.Warmup
+{
+    [TestClass]
+    public class BirthdayCakeCandlesTest
+    {
+        private BirthdayCakeCandles _birthdayCakeCandles;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _birthdayCakeCandles = new BirthdayCakeCandles();
+        }
+
+        [TestMethod]
+        public void BirthdayCakeCandles_Example_Test()
+        {
+            var result = _birthdayCakeCandles.solution(4, new[] {3, 2, 1, 3});
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
